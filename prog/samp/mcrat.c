380e5988ee35daf34be7b4096c6726bebd24f5f9
Cheng Zhang <3ki5tj@gmail.com>
2013-07-21 00:29:16 -0500

    mcrat separate lookup and direct versions

--- prog/samp/mcrat.c
+++ prog/samp/mcrat.c
@@ -85,22 +85,76 @@ static void doargs(int argc, char **argv)
 
 
 
-/* compute the sign of the virial coefficient */
-static double mcrun(int n, double nequil, double nsteps,
-    double amp[2], int nstfb, int lookup,
-    int nstnmv, double *nrat, double *tacc)
+#define WTSYS2(wt, fb) if ((wt = abs(fb)) == 0) wt = 1;
+
+/* change `sys' between three systems of biconnected diagrams
+ * 1. all
+ * 2. those without clique separators
+ * 3. those with |fb| as the weight  */
+INLINE void movesys(int *sys, int fb, int nz, int ifb,
+    double Z[], double tacc[])
+{
+  double r;
+  int wt;
+
+  if (*sys == 0) { /* sys == 0 */
+    if ( nz ) { /* only a `nz' configuration can switch */
+      if (rnd0() < 0.5) { /* switch sys from 0 to 1 */
+        *sys = 1;
+      } else { /* switch sys from 0 to 2 */
+        if ( ifb ) {
+          WTSYS2(wt, fb);
+          if ((r = wt*Z[0]/Z[2]) >= 1 || rnd0() < r)
+            *sys = 2;
+        }
+      }
+    }
+  } else if (*sys == 1) { /* sys == 1 */
+    if (rnd0() < 0.5) { /* switch sys from 1 to 0 */
+      if ((r = Z[1]/Z[0]) >= 1 || rnd0() < r)
+        *sys = 0;
+    } else { /* switch sys from 1 to 2 */
+      if ( ifb ) {
+        tacc[0] += 1;
+        WTSYS2(wt, fb);
+        if ((r = wt*Z[1]/Z[2]) >= 1 || rnd0() < r) {
+          *sys = 2;
+          tacc[1] += 1;
+        }
+      }
+    }
+  } else { /* sys 2 */
+    if (rnd0() < 0.5) { /* switch sys from 2 to 1 */
+      tacc[2] += 1;
+      WTSYS2(wt, fb);
+      if ((r = Z[2]/(Z[1]*wt)) >= 1 || rnd0() < r) {
+        *sys = 1;
+        tacc[3] += 1;
+      }
+    } else { /* switch sys from 2 to 0 */
+      WTSYS2(wt, fb);
+      if ((r = Z[2]/(Z[0]*wt)) >= 1 || rnd0() < r)
+        *sys = 0;
+    }
+  }
+}
+
+
+
+/* compute the sign of the virial coefficient,
+ * assuming a lookup table */
+INLINE double mcrat_lookup(int n, double nequil, double nsteps,
+    double amp[], int nstnmv, double *nrat, double *tacc)
 {
   rvn_t *x, xi;
   int i, j, it, fb = 0, nfb = 0, wt, nwt, nz, nnz, nbc, eql;
   dg_t *g, *ng, *sg;
-  double fbnz, fbav, t, r;
+  double fbnz, fbav, t;
   double fbsum[NSYS], hist[NSYS], cacc[NSYS], ctot[NSYS];
   int sys, sys0, acc1;
-  int needfb = 0; /* need to compute fb */
-  int hasfb = 1; /* if fb has been computed for the step */
-  int hasnfb = 1; /* if fb has been computed for the MC trial */
-  int ifb = 1; /* if fb is randomly computed */
+  unqid_t gmapid;
 
+  die_if (n > DGMAP_NMAX, "no diagram map for n %d\n", n);
   xnew(x, n + 1);
   for (i = 0; i < n; i++)
     rvn_rnd(x[i], (real) (-0.05 / sqrt(D)), (real) (0.05 / sqrt(D)) );
@@ -109,10 +163,7 @@ static double mcrun(int n, double nequil, double nsteps,
   sg = dg_open(n - 1);
   mkgraph(g, x);
   die_if (!dg_biconnected(g), "initial diagram not biconnected D %d\n", D);
-
-  fb = lookup ? dg_hsfb(g) : dg_hsfbrjw(g);
-  hasfb = 1;
-
+  fb = dg_hsfb_lookup(g);
   nz = 1; /* if (lookup), nz means fb != 0,
              otherwise nz means no clique separator */
   sys = 0;
@@ -140,33 +191,13 @@ static double mcrun(int n, double nequil, double nsteps,
         dg_link(ng, i, j);
     }
 
-    if ( lookup ) { /* cheap lookup verseion */
-      unqid_t gmapid = dg_getmapid(ng);
-      nbc = dg_biconnected_lookuplow(n, gmapid);
-      /* the weight `fb' is computed from the lookup table
-       * for a small n, so it can be updated in every step */
-      nfb = nbc ? dg_hsfb_lookuplow(n, gmapid) : 0;
-      nnz = (nfb != 0);
-    } else { /* direct and expensive computation, large n */
-      nbc = dg_biconnected(ng);
-      /* since computing fb is expensive,
-       * we only try to find if a clique separator,
-       * if so, fb == 0, otherwise fb is likely nonzero
-       * so this is a good approximation of the ensemble */
-      nnz = nbc ? (dg_cliquesep(ng) == 0) : 0;
-      /* we have to compute `nfb' if `sys' == 3 */
-      nfb = 0;
-      hasnfb = needfb = (sys > 1);
-      if ( needfb ) { /* if we must compute the new fb */
-        /* if the connectivity is unchanged, use the old fb */
-        if (hasfb && g->c[i] == g->c[j])
-          nfb = fb;
-        else
-          nfb = dg_hsfbrjw(ng);
-      }
-    }
+    gmapid = dg_getmapid(ng);
+    nbc = dg_biconnected_lookuplow(n, gmapid);
+    /* the weight `fb' is computed from the lookup table
+     * for a small n, so it can be updated in every step */
+    nfb = nbc ? dg_hsfb_lookuplow(n, gmapid) : 0;
+    nnz = (nfb != 0);
 
-#define WTSYS2(wt, fb) if ((wt = abs(fb)) == 0) wt = 1;
     if (sys == 0) {
       acc1 = nbc;
     } else if (sys == 1) {
@@ -184,10 +215,168 @@ static double mcrun(int n, double nequil, double nsteps,
       rvn_copy(x[i], xi);
       dg_copy(g, ng);
       nz = nnz;
-      if ( lookup )
-        fb = nfb;
+      fb = nfb;
+    }
+
+    /* change `sys' */
+    movesys(&sys, fb, nz, 1, Z, tacc);
+
+    if (eql) {
+      if (t >= nequil) {
+        printf("equilibrated at t %g, nstfb %d\n", t, nstfb);
+        t = 0;
+        it = 0;
+        eql = 0;
+      }
+      continue;
+    }
+
+    cacc[sys0] += acc1;
+    ctot[sys0] += 1;
+
+    /* accumulate data after equilibration */
+    if (sys == 0) fbnz += nz;
+    hist[sys] += 1;
+    if (sys <= 1) {
+      fbsum[sys] += fb;
+    } else {
+      WTSYS2(wt, fb);
+      fbsum[sys] += fb/wt;
+    }
+
+    if (it % nstnmv == 0) { /* compute the acceptance rates */
+      rvn_rmcom(x, n); /* remove the origin to the center of mass */
+
+      if (sys == 0) {
+        if (rnd0() < 0.5) {
+          /* the rate of biconnectivity after adding a random vertex */
+          nrat[0] += 1;
+          nrat[1] += rvn_voladd(x, n, xi);
+        } else {
+          /* the rate of biconnectivity after removing a random vertex */
+          nrat[2] += 1;
+          nrat[3] += dgmc_nremove(g, sg, n, &i);
+        }
+      }
+    }
+
+#define REPORT_LOOKUP() { \
+      fbav = (fbsum[0] + fbsum[1]) / (1 + hist[1]/fbnz) / hist[0]; \
+      fprintf(stderr, "D %d, n %d, t%11g, cacc %.4f/%.4f/%.4f, " \
+          "fb %+.8f/%+.8f|%+.8f %+.8f, %d, id %d\n" \
+          "    nz %.7f, his1/0 %.7f, 2/1 %.7f; " \
+          "tacc %.7f %.7f; n+: %.7f, n-: %.7f\n", \
+        D, n, t, cacc[0]/ctot[0], cacc[1]/ctot[1], cacc[2]/ctot[2], \
+        fbsum[0]/hist[0], fbsum[1]/hist[1] * (hist[1]/hist[0]) * Z[1]/Z[0], fbav, \
+        fbsum[2]/hist[2] * (tacc[1]/tacc[0])/(tacc[3]/tacc[2]) * (Z[2]/Z[1]) * (fbnz/hist[0]), fb, \
+        (int) gmapid, fbnz/hist[0], hist[1]/hist[0], hist[2]/hist[1], \
+        tacc[1]/tacc[0], tacc[3]/tacc[2], nrat[1]/nrat[0], nrat[3]/nrat[2]); }
+
+    if (it % nstrep == 0) {
+      it = 0;
+      REPORT_LOOKUP();
+    }
+  }
+  free(x);
+  dg_close(g);
+  dg_close(ng);
+  dg_close(sg);
+
+  REPORT_LOOKUP();
+  return fbav;
+}
+
+
+
+/* compute the sign of the virial coefficient by importance sampling
+ * direct version without lookup table */
+INLINE double mcrat_direct(int n, double nequil, double nsteps,
+    double amp[], int nstfb, int nstnmv, double *nrat, double *tacc)
+{
+  rvn_t *x, xi;
+  int i, j, it, fb = 0, nfb = 0, wt, nwt, nz, nnz, nbc, eql;
+  dg_t *g, *ng, *sg;
+  double fbnz, fbav, t;
+  double fbsum[NSYS], hist[NSYS], cacc[NSYS], ctot[NSYS];
+  int sys, sys0, acc1;
+  int needfb = 0; /* need to compute fb */
+  int hasfb = 1; /* if fb has been computed for the step */
+  int hasnfb = 1; /* if fb has been computed for the MC trial */
+  int ifb = 1; /* if fb is randomly computed */
+
+  xnew(x, n + 1);
+  for (i = 0; i < n; i++)
+    rvn_rnd(x[i], (real) (-0.05 / sqrt(D)), (real) (0.05 / sqrt(D)) );
+  g = dg_open(n);
+  ng = dg_open(n);
+  sg = dg_open(n - 1);
+  mkgraph(g, x);
+  die_if (!dg_biconnected(g), "initial diagram not biconnected D %d\n", D);
+
+  fb = dg_hsfbrjw(g);
+  hasfb = 1;
+  nz = 1; /* if (lookup), nz means fb != 0,
+             otherwise nz means no clique separator */
+  sys = 0;
+  eql = 1;
+  nrat[0] = nrat[2] = 1e-6; nrat[1] = nrat[3] = 0;
+  tacc[0] = tacc[2] = 1e-6; tacc[1] = tacc[3] = 0;
+  for (i = 0; i < NSYS; i++) {
+    fbsum[i] = 0;
+    hist[i] = 1e-6;
+    cacc[i] = 0;
+    ctot[i] = 1e-6;
+  }
+
+  for (it = 1, t = 1; t <= nsteps; t += 1, it++) {
+    sys0 = sys;
+
+    i = (int) (rnd0() * n);
+    rvn_rnddisp(xi, x[i], amp[sys]);
+    dg_copy(ng, g);
+    for (j = 0; j < n; j++) {
+      if (j == i) continue;
+      if (rvn_dist2(xi, x[j]) >= 1)
+        dg_unlink(ng, i, j);
       else
-        hasfb = hasnfb;
+        dg_link(ng, i, j);
+    }
+
+    nbc = dg_biconnected(ng);
+    /* since computing fb is expensive,
+     * we only try to find if a clique separator,
+     * if so, fb == 0, otherwise fb is likely nonzero
+     * so this is a good approximation of the ensemble */
+    nnz = nbc ? (dg_cliquesep(ng) == 0) : 0;
+    /* we have to compute `nfb' if `sys' == 3 */
+    nfb = 0;
+    hasnfb = needfb = (sys > 1);
+    if ( needfb ) { /* if we must compute the new fb */
+      /* if the connectivity is unchanged, use the old fb */
+      if (hasfb && g->c[i] == g->c[j])
+        nfb = fb;
+      else
+        nfb = dg_hsfbrjw(ng);
+    }
+    
+    if (sys == 0) {
+      acc1 = nbc;
+    } else if (sys == 1) {
+      acc1 = nnz;
+    } else { /* sys == 2 */
+      if (nnz) {
+        WTSYS2(wt, fb);
+        WTSYS2(nwt, nfb);
+        if (nwt >= wt) acc1 = 1;
+        else acc1 = (rnd0() < 1.*nwt/wt);
+      } else acc1 = 0;
+    }
+
+    if ( acc1 ) { /* accept the move */
+      rvn_copy(x[i], xi);
+      dg_copy(g, ng);
+      nz = nnz;
+      hasfb = hasnfb;
     } else {
       /* if rejected, maintain the old `hasfb' value */
     }
@@ -200,50 +389,11 @@ static double mcrun(int n, double nequil, double nsteps,
     }
 
     /* change `sys' */
-    if (sys == 0) { /* `sys' 0 */
-      if ( nz ) { /* only a `nz' configuration can switch */
-        if (rnd0() < 0.5) { /* switch `sys' from 0 to 1 */
-          sys = 1;
-        } else { /* switch `sys' from 0 to 2 */
-          if ( ifb ) {
-            WTSYS2(wt, fb);
-            if ((r = wt*Z[0]/Z[2]) >= 1 || rnd0() < r)
-              sys = 2;
-          }
-        }
-      }
-    } else if (sys == 1) { /* `sys' 1 */
-      if (rnd0() < 0.5) { /* switch `sys' from 1 to 0 */
-        if ((r = Z[1]/Z[0]) >= 1 || rnd0() < r)
-          sys = 0;
-      } else { /* switch `sys' from 1 to 2 */
-        if ( ifb ) {
-          tacc[0] += 1;
-          WTSYS2(wt, fb);
-          if ((r = wt*Z[1]/Z[2]) >= 1 || rnd0() < r) {
-            sys = 2;
-            tacc[1] += 1;
-          }
-        }
-      }
-    } else { /* `sys' 2 */
-      if (rnd0() < 0.5) { /* switch `sys' from 2 to 1 */
-        tacc[2] += 1;
-        WTSYS2(wt, fb);
-        if ((r = Z[2]/(Z[1]*wt)) >= 1 || rnd0() < r) {
-          sys = 1;
-          tacc[3] += 1;
-        }
-      } else { /* switch `sys' from 2 to 0 */
-        WTSYS2(wt, fb);
-        if ((r = Z[2]/(Z[0]*wt)) >= 1 || rnd0() < r)
-          sys = 0;
-      }
-    }
+    movesys(&sys, fb, nz, ifb, Z, tacc);
 
     if (eql) {
       if (t >= nequil) {
-        printf("equilibrated at t %g, nstfb %d, lookup %d\n", t, nstfb, lookup);
+        printf("equilibrated at t %g, nstfb %d\n", t, nstfb);
         t = 0;
         it = 0;
         eql = 0;
@@ -255,7 +405,7 @@ static double mcrun(int n, double nequil, double nsteps,
     ctot[sys0] += 1;
 
     /* accumulate data after equilibration */
-    if (lookup || ifb || sys0 >= 2) {
+    if (ifb || sys0 >= 2) {
       if (sys == 0) fbnz += nz;
       hist[sys] += 1;
       if (sys <= 1) {
@@ -282,7 +432,7 @@ static double mcrun(int n, double nequil, double nsteps,
       }
     }
 
-#define REPORT() { \
+#define REPORT_DIRECT() { \
       fbav = (fbsum[0] + fbsum[1]) / (1 + hist[1]/fbnz) / hist[0]; \
       fprintf(stderr, "D %d, n %d, t%11g, cacc %.4f/%.4f/%.4f, " \
           "fb %+.8f/%+.8f|%+.8f %+.8f, %d\n" \
@@ -296,7 +446,7 @@ static double mcrun(int n, double nequil, double nsteps,
 
     if (it % nstrep == 0) {
       it = 0;
-      REPORT();
+      REPORT_DIRECT();
     }
   }
   free(x);
@@ -304,8 +454,21 @@ static double mcrun(int n, double nequil, double nsteps,
   dg_close(ng);
   dg_close(sg);
 
-  REPORT();
+  REPORT_DIRECT();
   return fbav;
+}
+
+
+
+/* compute the sign of the virial coefficient */
+static double mcrat(int n, double nequil, double nsteps,
+    double amp[], int nstfb, int lookup, int nstnmv, double *nrat, double *tacc)
+{
+  printf("D %d, n %d, %s version\n", D, n, lookup ? "lookup" : "direct");
+  if (lookup)
+    return mcrat_lookup(n, nequil, nsteps, amp, nstnmv, nrat, tacc);
+  else
+    return mcrat_direct(n, nequil, nsteps, amp, nstfb, nstnmv, nrat, tacc);
 }
 
 
@@ -319,9 +482,9 @@ int main(int argc, char **argv)
       D, n, (double) nsteps, nsteps2, mcamp[0], mcamp[1],
       nstfb, (int) sizeof(code_t) * 8);
 
-  fbav1 = mcrun(n, nequil, nsteps, mcamp,
+  fbav1 = mcrat(n, nequil, nsteps, mcamp,
       nstfb, lookup, nstnmv, nrat1, tr1);
-  fbav2 = mcrun(n - 1, nequil, nsteps2, mcamp,
+  fbav2 = mcrat(n - 1, nequil, nsteps2, mcamp,
       nstfb, lookup, nstnmv, nrat2, tr2);
   nr1 = nrat1[3]/nrat1[2];
   nr2 = nrat2[1]/nrat2[0];
